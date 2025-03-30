9c4d51875fc9d4617733b249ff620783d730cda9
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-03-31 02:45:44 +0800

    refactor: reabstract function for upload care uploader

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,64 +1,66 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { OutputCollectionState } from "@uploadcare/react-uploader";
+import "@uploadcare/react-uploader/core.css";
+import { useState } from "react";
+import { dataResult } from "./api/uploadcare-api/list-of-files";
 import { getData } from "./api/classify-image";
-import Uploader from "@/components/uploadcare/uploader";
-
-const formSchema = z.object({
-	link: z.string(),
-});
+import Image from "next/image";
+import { FileUploaderRegular } from "@uploadcare/react-uploader/next";
 
 export default function Home() {
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			link: "",
-		},
-	});
+	const [data, setData] = useState<string | null>("");
+	const [onLoad, setOnLoad] = useState<boolean>(false);
 
-	async function onSubmit(values: z.infer<typeof formSchema>) {
-		console.log("Sending...");
+	const isDone = async (e: OutputCollectionState) => {
+		// console.log(e.allEntries[0].cdnUrl);
 		try {
-			const posts = await getData({ image_url: values.link });
+			setOnLoad(true);
+			const dt = await dataResult({ uuid: e.allEntries[0].uuid });
+			setData(dt);
+
+			const posts = await getData({ image_url: dt || "" });
 			console.log(posts.diagnosis);
 		} catch (error) {
 			console.log(error);
 		} finally {
-			console.log("DONE SENDING!");
+			setOnLoad(false);
 		}
-	}
+	};
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center">
 				<h1 className="text-2xl font-bold">PawMed AI V2.0</h1>
 
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className="flex items-center gap-3"
-					>
-						<FormField
-							control={form.control}
-							name="link"
-							render={({ field }) => (
-								<FormItem>
-									<FormControl>
-										<Input placeholder="shadcn" className="w-96" {...field} />
-									</FormControl>
-								</FormItem>
-							)}
-						/>
-						<Button type="submit">Submit</Button>
-					</form>
+				<div>
+					<FileUploaderRegular
+						onDoneClick={isDone}
+						sourceList="local, camera, facebook"
+						cameraModes="photo"
+						classNameUploader="uc-light"
+						pubkey="d64bdc6f78d22cc47060"
+					/>
 
-					<Uploader />
-				</Form>
+					{!onLoad ? (
+						data && <Image src={data} alt={data} width={500} height={500} />
+					) : (
+						<div>Loading...</div>
+					)}
+
+					{/* {images.map(
+                  (i, idx) =>
+                    i.originalFileUrl && (
+                      <Image
+                        key={idx}
+                        src={i.originalFileUrl}
+                        alt={i.originalFilename}
+                        width={500}
+                        height={500}
+                      />
+                    )
+                )} */}
+				</div>
 			</main>
 		</div>
 	);
