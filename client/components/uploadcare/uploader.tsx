09c06b41483c5c3f26d64ef2e9988c3b89be639f
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-03-31 02:41:15 +0800

    chore: integrate with LLM

--- client/components/uploadcare/uploader.tsx
+++ client/components/uploadcare/uploader.tsx
@@ -4,17 +4,15 @@ import { FileUploaderRegular } from "@uploadcare/react-uploader/next";
 import "@uploadcare/react-uploader/core.css";
 import { OutputCollectionState } from "@uploadcare/react-uploader";
 import Image from "next/image";
-import {
-	dataResult,
-	loadAllData,
-} from "@/app/api/uploadcare-api/list-of-files";
+import { dataResult } from "@/app/api/uploadcare-api/list-of-files";
 import { useState } from "react";
-import { FileInfo } from "@uploadcare/rest-client";
+import { getData } from "@/app/api/classify-image";
+// import { FileInfo } from "@uploadcare/rest-client";
 
 function Uploader() {
 	const [data, setData] = useState<string | null>("");
 	const [onLoad, setOnLoad] = useState<boolean>(false);
-	const [images, setImages] = useState<FileInfo[]>([]);
+	// const [images, setImages] = useState<FileInfo[]>([]);
 
 	const isDone = async (e: OutputCollectionState) => {
 		// console.log(e.allEntries[0].cdnUrl);
@@ -22,6 +20,9 @@ function Uploader() {
 			setOnLoad(true);
 			const dt = await dataResult({ uuid: e.allEntries[0].uuid });
 			setData(dt);
+
+			const posts = await getData({ image_url: dt || "" });
+			console.log(posts.diagnosis);
 		} catch (error) {
 			console.log(error);
 		} finally {
@@ -29,35 +30,34 @@ function Uploader() {
 		}
 	};
 
-	const load = async () => {
-		const allData = await loadAllData();
+	// const load = async () => {
+	// 	const allData = await loadAllData();
 
-		console.log(allData);
-		setImages(allData.results);
+	// 	console.log(allData);
+	// 	setImages(allData.results);
 
-		allData.results.map((result) => {
-			console.log(result.originalFileUrl);
-		});
-	};
+	// 	allData.results.map((result) => {
+	// 		console.log(result.originalFileUrl);
+	// 	});
+	// };
 
 	return (
 		<div>
 			<FileUploaderRegular
 				onDoneClick={isDone}
-				onModalOpen={load}
 				sourceList="local, camera, facebook"
 				cameraModes="photo"
 				classNameUploader="uc-light"
 				pubkey="d64bdc6f78d22cc47060"
 			/>
 
-			{/* {!onLoad ? (
+			{!onLoad ? (
 				data && <Image src={data} alt={data} width={500} height={500} />
 			) : (
 				<div>Loading...</div>
-			)} */}
+			)}
 
-			{images.map(
+			{/* {images.map(
 				(i, idx) =>
 					i.originalFileUrl && (
 						<Image
@@ -68,7 +68,7 @@ function Uploader() {
 							height={500}
 						/>
 					)
-			)}
+			)} */}
 		</div>
 	);
 }
