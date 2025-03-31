25aebe5d8c9201cbf068e4e1b7e4736e8b0bb541
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-03-31 23:04:26 +0800

    chore: install shadcn themes

--- client/app/layouts/header.tsx
+++ client/app/layouts/header.tsx
@@ -3,7 +3,13 @@ import { CustomNavigationMenu } from "@/components/shared/navigation";
 export const HeaderLayout = () => {
 	return (
 		<div className="w-full fixed top-0 py-3 px-24 z-10 border border-b">
-			<CustomNavigationMenu />
+			<div className="flex justify-between items-center">
+				<div>
+					<h2 className="text-xl font-bold">Pawmed AI V2.0</h2>
+				</div>
+
+				<CustomNavigationMenu />
+			</div>
 		</div>
 	);
 };
