2206125b4071ddc600fcbc92622092f1c73e8520
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-03-31 23:08:29 +0800

    fix: fix theme hydration error chore: add toggle for theme

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { HeaderLayout } from "./layouts/header";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -28,8 +29,15 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<HeaderLayout />
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<HeaderLayout />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
