53d1c8465e3e76b7e3ebe0f7f161e94c5cbe98ff
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-01 03:01:53 +0800

    chore: add pause on hover on marquee

--- client/app/(pages)/test/page.tsx
+++ client/app/(pages)/test/page.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils";
 import { Marquee } from "@/components/magicui/marquee";
-import Image from "next/image";
 
 const reviews = [
 	{
@@ -21,24 +20,6 @@ const reviews = [
 		body: "I'm at a loss for words. This is amazing. I love it.",
 		img: "https://avatar.vercel.sh/john",
 	},
-	{
-		name: "Jane",
-		username: "@jane",
-		body: "I'm at a loss for words. This is amazing. I love it.",
-		img: "https://avatar.vercel.sh/jane",
-	},
-	{
-		name: "Jenny",
-		username: "@jenny",
-		body: "I'm at a loss for words. This is amazing. I love it.",
-		img: "https://avatar.vercel.sh/jenny",
-	},
-	{
-		name: "James",
-		username: "@james",
-		body: "I'm at a loss for words. This is amazing. I love it.",
-		img: "https://avatar.vercel.sh/james",
-	},
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
@@ -58,20 +39,15 @@ const ReviewCard = ({
 	return (
 		<figure
 			className={cn(
-				"relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-				"shrink-0",
+				"relative h-full w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
+				// light styles
 				"border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+				// dark styles
 				"dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
 			)}
 		>
 			<div className="flex flex-row items-center gap-2">
-				<Image
-					className="rounded-full"
-					width="32"
-					height="32"
-					alt=""
-					src={img}
-				/>
+				<img className="rounded-full" width="32" height="32" alt="" src={img} />
 				<div className="flex flex-col">
 					<figcaption className="text-sm font-medium dark:text-white">
 						{name}
@@ -84,21 +60,21 @@ const ReviewCard = ({
 	);
 };
 
-export default function MarqueeDemo() {
+export default function MarqueeDemoVertical() {
 	return (
-		<div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-			<Marquee pauseOnHover className="[--duration:20s]">
+		<div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden">
+			<Marquee pauseOnHover vertical className="[--duration:5s]">
 				{firstRow.map((review) => (
 					<ReviewCard key={review.username} {...review} />
 				))}
 			</Marquee>
-			<Marquee reverse pauseOnHover className="[--duration:20s]">
+			<Marquee reverse pauseOnHover vertical className="[--duration:5s]">
 				{secondRow.map((review) => (
 					<ReviewCard key={review.username} {...review} />
 				))}
 			</Marquee>
-			<div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-			<div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+			<div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div>
+			<div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-background"></div>
 		</div>
 	);
 }
