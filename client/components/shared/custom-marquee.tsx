f8f206376991c8674157c31e65c81fdba19b8870
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-01 20:59:47 +0800

    refactor: change the image of marquee card

--- client/components/shared/custom-marquee.tsx
+++ client/components/shared/custom-marquee.tsx
@@ -4,65 +4,58 @@ import Image from "next/image";
 
 const reviews = [
 	{
-		name: "Jack",
-		username: "@jack",
-		body: "I've never seen anything like this before. It's amazing. I love it.",
-		img: "https://avatar.vercel.sh/jack",
+		img: "/dogs/1.png",
 	},
 	{
-		name: "Jill",
-		username: "@jill",
-		body: "I don't know what to say. I'm speechless. This is amazing.",
-		img: "https://avatar.vercel.sh/jill",
+		img: "/dogs/1.png",
 	},
 	{
-		name: "John",
-		username: "@john",
-		body: "I'm at a loss for words. This is amazing. I love it.",
-		img: "https://avatar.vercel.sh/john",
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
+	},
+	{
+		img: "/dogs/1.png",
 	},
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
-const ReviewCard = ({
-	img,
-	name,
-	username,
-	body,
-}: {
-	img: string;
-	name: string;
-	username: string;
-	body: string;
-}) => {
+const ReviewCard = ({ img }: { img: string }) => {
 	return (
 		<figure
 			className={cn(
-				"relative h-full w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
+				"relative h-full w-36 cursor-pointer overflow-hidden rounded-xl border",
 				// light styles
 				"border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
 				// dark styles
 				"dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
 			)}
 		>
-			<div className="flex flex-row items-center gap-2">
-				<Image
-					className="rounded-full"
-					width="32"
-					height="32"
-					alt=""
-					src={img}
-				/>
-				<div className="flex flex-col">
-					<figcaption className="text-sm font-medium dark:text-white">
-						{name}
-					</figcaption>
-					<p className="text-xs font-medium dark:text-white/40">{username}</p>
-				</div>
-			</div>
-			<blockquote className="mt-2 text-sm">{body}</blockquote>
+			<Image width="200" height="200" alt="" src={img} />
 		</figure>
 	);
 };
@@ -71,18 +64,18 @@ export const CustomMarqueeCard = () => {
 	return (
 		<div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden">
 			<Marquee pauseOnHover vertical className="[--duration:10s]">
-				{firstRow.map((review) => (
-					<ReviewCard key={review.username} {...review} />
+				{firstRow.map((review, idx) => (
+					<ReviewCard key={idx} {...review} />
 				))}
 			</Marquee>
 			<Marquee reverse pauseOnHover vertical className="[--duration:10s]">
-				{secondRow.map((review) => (
-					<ReviewCard key={review.username} {...review} />
+				{secondRow.map((review, idx) => (
+					<ReviewCard key={idx} {...review} />
 				))}
 			</Marquee>
 			<Marquee pauseOnHover vertical className="[--duration:10s]">
-				{secondRow.map((review) => (
-					<ReviewCard key={review.username} {...review} />
+				{secondRow.map((review, idx) => (
+					<ReviewCard key={idx} {...review} />
 				))}
 			</Marquee>
 			<div className="pointer-events-none absolute inset-x-0 top-0 h-1/4 bg-gradient-to-b from-background"></div>
