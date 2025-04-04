2177872cea85bab124c37093d3191121ba2dccbe
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-04 23:26:06 +0800

    refactor: change base url

--- client/app/api/classify-image.ts
+++ client/app/api/classify-image.ts
@@ -7,7 +7,7 @@ type DataProps = {
 async function getData({ image_url }: DataProps) {
 	// await new Promise((resolve) => setTimeout(resolve, 3000));
 	const res = await fetch(
-		`http://localhost:3000/classifyDss?img_url=${image_url}`,
+		`https://9187-119-93-27-127.ngrok-free.app/classifyDss?img_url=${image_url}`,
 		{
 			method: "POST",
 		}
