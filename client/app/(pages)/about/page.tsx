35397b8c52ac7edbe80671f28af1e6293a4b42fc
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-05 11:23:44 +0800

    chore: integrate andorid component add heading -> about page

--- client/app/(pages)/about/page.tsx
+++ client/app/(pages)/about/page.tsx
@@ -1,7 +1,18 @@
+import { CustomAndroid } from "@/components/shared/custom-android";
+
 export default function About() {
 	return (
-		<div className="md:py-5 py-14">
-			<h1 className="text-2xl">About Page</h1>
+		<div className="flex w-full md:py-5 py-14">
+			<div className="w-full">
+				<h5 className="text-sm font-semibold tracking-widest">ABOUT US</h5>
+				<h1 className="text-5xl font-bold text-[#FF7800] leading-14">
+					Revolutionizing Pet Healthcare with Artificial Intelligence
+				</h1>
+			</div>
+
+			<div>
+				<CustomAndroid />
+			</div>
 		</div>
 	);
 }
---
+++ client/components/shared/custom-android.tsx
@@ -0,0 +1,9 @@
+import Android from "@/components/magicui/android";
+
+export function CustomAndroid() {
+	return (
+		<div className="relative">
+			<Android className="size-96" />
+		</div>
+	);
+}
