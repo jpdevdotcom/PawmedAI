0f5bbfae41a0770c729ca96e6608538e4a412dd6
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-05 18:49:39 +0800

    fix: fix the background of header in dark mode

--- client/app/layouts/header.tsx
+++ client/app/layouts/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const HeaderLayout = () => {
 	return (
-		<div className="w-full fixed top-0 py-3 md:px-24 px-7 z-10 border border-b bg-white">
+		<div className="w-full fixed top-0 py-3 md:px-24 px-7 z-10 border border-b bg-white dark:bg-gray-950">
 			<div className="flex justify-between items-center">
 				<div>
 					{/* <h2 className="text-xl font-bold">Pawmed AI V2.0</h2> */}
