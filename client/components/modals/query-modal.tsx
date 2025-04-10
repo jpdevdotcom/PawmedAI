6f8e9c20b17db2479077d8a213999afbc621c465
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-10 23:01:12 +0800

    chore: add form -> query modal

--- client/components/modals/query-modal.tsx
+++ client/components/modals/query-modal.tsx
@@ -1,8 +1,57 @@
 import { useQueryModal } from "@/hooks/modal-trigger";
 import { Modal } from "../ui/modal";
+import { useForm } from "react-hook-form";
+import { QuerySchema } from "@/schema/query-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+
+type queryModalProps = {
+	name: string;
+	label: string;
+	placeholder: string;
+};
+
+const queryModalData: queryModalProps[] = [
+	{
+		name: "firstname",
+		label: "Firstname",
+		placeholder: "Enter your firstname...",
+	},
+	{
+		name: "lastname",
+		label: "Lastname",
+		placeholder: "Enter your lastname...",
+	},
+	{
+		name: "emailAddress",
+		label: "Email Address",
+		placeholder: "e.g. juandelacruz@gmail.com",
+	},
+	{
+		name: "feedback",
+		label:
+			"Please give as much detail as you can and share examples if you have any:",
+		placeholder: "Enter your thoughts...",
+	},
+];
 
 export const QueryModal = () => {
 	const queryModal = useQueryModal();
+
+	const form = useForm<z.infer<typeof QuerySchema>>({
+		resolver: zodResolver(QuerySchema),
+		defaultValues: {
+			firstname: "",
+			lastname: "",
+			feedback: "",
+		},
+	});
+
+	async function onSubmitQuery(values: z.infer<typeof QuerySchema>) {
+		console.log(values);
+	}
 
 	return (
 		<Modal
@@ -12,7 +61,24 @@ export const QueryModal = () => {
 			onClose={queryModal.onClose}
 			modalWidth="sm:max-w-[50em]"
 		>
-			Query Modal
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmitQuery)}>
+					{queryModalData.map((query, idx) => (
+						<FormField
+							key={idx}
+							name={query.name}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>{query.label}</FormLabel>
+									<FormControl>
+										<Input placeholder={query.placeholder} {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					))}
+				</form>
+			</Form>
 		</Modal>
 	);
 };
