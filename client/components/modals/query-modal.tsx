94e7eeeb68c449c5b54887afe24556d38c61ebde
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-10 23:10:36 +0800

    chore: polish design -> query modal

--- client/components/modals/query-modal.tsx
+++ client/components/modals/query-modal.tsx
@@ -4,9 +4,18 @@ import { useForm } from "react-hook-form";
 import { QuerySchema } from "@/schema/query-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "../ui/form";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 type queryModalProps = {
 	name: string;
@@ -52,6 +61,17 @@ export const QueryModal = () => {
 
 	async function onSubmitQuery(values: z.infer<typeof QuerySchema>) {
 		console.log(values);
+		resetForm();
+	}
+
+	function resetForm() {
+		form.reset({
+			firstname: "",
+			lastname: "",
+			emailAddress: "",
+			feedback: "",
+		});
+		queryModal.onClose();
 	}
 
 	return (
@@ -64,28 +84,67 @@ export const QueryModal = () => {
 		>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmitQuery)} className="space-y-5">
-					{queryModalData.map((query, idx) => (
-						<FormField
-							key={idx}
-							name={query.name}
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel>{query.label}</FormLabel>
-									<FormControl>
-										{query.name === "feedback" ? (
-											<Textarea
-												placeholder={query.placeholder}
-												{...field}
-												className="resize-none h-36"
-											/>
-										) : (
-											<Input placeholder={query.placeholder} {...field} />
-										)}
-									</FormControl>
-								</FormItem>
-							)}
+					<div className="flex justify-center items-center gap-3 bg-gray-100 p-7 rounded-lg">
+						<Image
+							src="/pawlogo.png"
+							alt="PawMed AI Logo"
+							width={60}
+							height={60}
 						/>
-					))}
+
+						<div>
+							<h3 className="text-lg font-bold">We value your suggestions.</h3>
+							<p className="text-sm">
+								We appreciate you taking the time to share your thoughts.
+								What&apos;s your suggestion?
+							</p>
+						</div>
+					</div>
+
+					<div className="grid grid-cols-2 gap-3 space-y-3">
+						{queryModalData.map((query, idx) => (
+							<FormField
+								key={idx}
+								name={query.name as keyof z.infer<typeof QuerySchema>}
+								render={({ field }) => (
+									<FormItem
+										className={query.name === "feedback" ? "col-span-2" : ""}
+									>
+										<FormLabel>{query.label}</FormLabel>
+										<FormControl>
+											{query.name === "feedback" ? (
+												<Textarea
+													placeholder={query.placeholder}
+													{...field}
+													className="resize-none h-36"
+												/>
+											) : (
+												<Input placeholder={query.placeholder} {...field} />
+											)}
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						))}
+					</div>
+
+					<div className="flex justify-end gap-3">
+						<Button
+							type="button"
+							variant={"outline"}
+							className="px-5 cursor-pointer"
+							onClick={() => resetForm()}
+						>
+							Cancel
+						</Button>
+						<Button
+							type="submit"
+							className="px-5 cursor-pointer hover:bg-gray-700"
+						>
+							Submit Feedback
+						</Button>
+					</div>
 				</form>
 			</Form>
 		</Modal>
