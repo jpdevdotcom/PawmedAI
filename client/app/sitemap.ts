c478ec498309fd031e4fdea8f1734be811d23fd1
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-12 01:40:18 +0800

    refactor: change back the sitemap

---
+++ client/app/sitemap.ts
@@ -0,0 +1,16 @@
+export default async function sitemap() {
+	return [
+		{
+			url: "https://pawmed-ai-v2-0.vercel.app",
+			lastModified: new Date(),
+		},
+		{
+			url: "https://pawmed-ai-v2-0.vercel.app/about",
+			lastModified: new Date(),
+		},
+		{
+			url: "https://pawmed-ai-v2-0.vercel.app/classify-disease",
+			lastModified: new Date(),
+		},
+	];
+}
