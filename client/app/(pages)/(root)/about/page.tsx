1af8499aee91deda5588841f23bc30e3e944684c
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-04-12 17:26:46 +0800

    refactor: change the metadata of each page for beter SEO

--- client/app/(pages)/(root)/about/page.tsx
+++ client/app/(pages)/(root)/about/page.tsx
@@ -3,85 +3,12 @@ import { CustomNumberTicker } from "@/components/shared/custom-number-ticker";
 import { Metadata } from "next";
 import Image from "next/image";
 
-export const metadata: Metadata = {
-	metadataBase: new URL("https://pawmed-ai-v2-0.vercel.app"),
-	verification: {
-		google: "V-geZ47dJfxUYZMo_BFsw6SacQZ27dCr8FtFR5JOeRE",
-	},
-	keywords: [
-		"PawMed AI",
-		"pawmed ai",
-		"pawmed",
-		"paw",
-		"med",
-		"AI",
-		"Pet Health",
-		"Veterinary",
-		"Veterinary AI",
-		"Animal Healthcare",
-		"Pet Diagnosis",
-		"AI Diagnosis",
-		"Veterinary Medicine",
-		"Veterinary Technology",
-		"Veterinary Assistant",
-		"Vet App",
-		"Pet App",
-		"Pet Health App",
-		"AI for Pets",
-		"Machine Learning",
-		"Deep Learning",
-		"Computer Vision",
-		"Image Classification",
-		"Vet Disease Classifier",
-		"Animal Disease Detection",
-		"Pet Disease Detection",
-		"programming",
-		"coding",
-		"Next.js",
-		"NestJS",
-		"React",
-		"TypeScript",
-		"Tailwind CSS",
-		"OpenAI",
-		"AI Project",
-		"Full Stack App",
-		"Web Development",
-		"Tech for Vets",
-		"Digital Vet Tools",
-	],
-	title: {
-		default: "PawMed AI",
-		template: "%s | jpdevdotcom-",
-	},
-	openGraph: {
-		title: "PawMed AI",
-		description: "Revolutionizing Pet Healthcare with Artificial Intelligence",
-		siteName: "PawMed AI",
-		url: "https://pawmed-ai-v2-0.vercel.app",
-		type: "website",
-		locale: "en_US",
-		images: [
-			{
-				url: "/pawlogo.png",
-				width: 1200,
-				height: 1200,
-				alt: "PawMed AI",
-				type: "image/png",
-			},
-		],
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "PawMed AI",
-		description: "Revolutionizing Pet Healthcare with Artificial Intelligence",
-		images: ["/pawlogo.png"],
-	},
-	icons: {
-		icon: ["/assets/favicon.ico?v=4"],
-		apple: ["/assets/apple-touch-icon.png?v=4"],
-		shortcut: ["/assets/apple-touch-icon.png"],
-	},
-};
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "About PawMed AI",
+		description: "Get to know us more!",
+	};
+}
 
 type Statistics = {
 	label: string;
--- client/app/sitemap.ts
+++ client/app/sitemap.ts
@@ -1,16 +1,24 @@
-export default async function sitemap() {
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	return [
 		{
 			url: "https://pawmed-ai-v2-0.vercel.app",
 			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.9,
 		},
 		{
 			url: "https://pawmed-ai-v2-0.vercel.app/about",
 			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.8,
 		},
 		{
 			url: "https://pawmed-ai-v2-0.vercel.app/classify-disease",
 			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 1,
 		},
 	];
 }
